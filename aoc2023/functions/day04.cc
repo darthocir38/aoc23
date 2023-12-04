7aa8cd3bbcf9aa53af9268b78f7c1a83aa4a6b9a
Rico Backasch <rico.backasch@fabmatics.com>
2023-12-04 08:32:51 +0100

    d4p1

--- aoc2023/functions/day04.cc
+++ aoc2023/functions/day04.cc
@@ -1,27 +1,78 @@
 
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <span>
 #include <sstream>
 #include <string>
+#include <string_view>
 
-namespace d04 {
+namespace d04
+{
 
-int p1(const char *data) {
-  std::stringstream stream{data};
-  std::string line;
-  int sum = 0;
-  while (std::getline(stream, line, '\n')) {
-    //sum += p1_get_num_of_line(line);
-  }
-  return sum;
+std::pair<signed, std::string> get_card(std::string const& line)
+{
+	auto colon_idx = line.find(':');
+	auto id_str    = line.substr(0, colon_idx);
+	return { std::stoi(id_str.substr(id_str.find(' '), id_str.size())),
+		     line.substr(colon_idx + 1, line.size()) };
 }
 
-int p2(const char *data) {
-  std::stringstream stream{data};
-  std::string line;
-  int sum = 0;
-  while (std::getline(stream, line, '\n')) {
-    //sum += p2_get_num_of_line(line);
-  }
-  return sum;
+std::pair<std::string, std::string> splitt_card(std::string const& card)
+{
+	auto pipe_idx  = card.find('|');
+	auto winners   = card.substr(0, pipe_idx);
+	auto scratched = card.substr(pipe_idx + 1, card.size());
+
+	return { winners, scratched };
 }
 
-} // namespace d4
+int p1_get_num_of_line(std::string const& line)
+{
+	auto const& [game_id, card_str]  = get_card(line);
+	auto const& [winners, scratched] = splitt_card(card_str);
+
+	auto value = 0;
+
+	for (auto i = 0; i < scratched.size(); i += 3)
+	{
+		std::string_view num{ begin(scratched) + i,
+			                  std::begin(scratched) + i + 3 };
+		if (winners.find(num) == std::string::npos) { continue; }
+		if (value == 0)
+		{
+			value = 1;
+			continue;
+		}
+		value *= 2;
+	}
+
+	return value;
+}
+
+int p1(const char* data)
+{
+	std::stringstream stream{ data };
+	std::string       line;
+	int               sum = 0;
+	while (std::getline(stream, line, '\n'))
+	{
+		sum += p1_get_num_of_line(line);
+	}
+	return sum;
+}
+
+int p2(const char* data)
+{
+	std::stringstream stream{ data };
+	std::string       line;
+	int               sum = 0;
+	while (std::getline(stream, line, '\n'))
+	{
+		// sum += p2_get_num_of_line(line);
+	}
+	return sum;
+}
+
+} // namespace d04
--- aoc2023/test.cc
+++ aoc2023/test.cc
@@ -1,5 +1,6 @@
 
 #include <catch2/catch_test_macros.hpp>
+#include <spdlog/spdlog.h>
 
 #include "functions.h"
 
@@ -76,4 +77,29 @@ TEST_CASE("D3P2") {
 .664.598..)";
 
   REQUIRE(d03::p2(data) == 467835);
+}
+
+TEST_CASE("D4P1") {
+  spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+  auto data =
+      R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)";
+
+  REQUIRE(d04::p1(data) == 13);
+}
+
+TEST_CASE("D4P2") {
+  auto data =
+      R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)";
+
+  // REQUIRE(d04::p2(data) == 13);
 }
