e21d259683aa202131ec8da196004afbe6322f6b
Rico Backasch <rico.backasch@fabmatics.com>
2023-12-04 08:57:39 +0100

    d4p2

--- aoc2023/functions/day04.cc
+++ aoc2023/functions/day04.cc
@@ -2,11 +2,14 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <numeric>
 #include <ranges>
 #include <span>
+#include <spdlog/spdlog.h>
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace d04
 {
@@ -51,6 +54,24 @@ int p1_get_num_of_line(std::string const& line)
 	return value;
 }
 
+int get_winners(std::string const& line)
+{
+	auto const& [game_id, card_str]  = get_card(line);
+	auto const& [winners, scratched] = splitt_card(card_str);
+
+	auto count = 0;
+
+	for (auto i = 0; i < scratched.size(); i += 3)
+	{
+		std::string_view num{ begin(scratched) + i,
+			                  std::begin(scratched) + i + 3 };
+		if (winners.find(num) == std::string::npos) { continue; }
+		count++;
+	}
+
+	return count;
+}
+
 int p1(const char* data)
 {
 	std::stringstream stream{ data };
@@ -67,12 +88,23 @@ int p2(const char* data)
 {
 	std::stringstream stream{ data };
 	std::string       line;
-	int               sum = 0;
+	std::vector<int>  winners;
 	while (std::getline(stream, line, '\n'))
 	{
-		// sum += p2_get_num_of_line(line);
+		winners.push_back(get_winners(line));
 	}
-	return sum;
+	std::vector<int> amount(winners.size(), 1);
+	for (auto i = 0; i < winners.size(); i++)
+	{
+		spdlog::debug(
+		    "line: {}, winners: {} amount: {}", i, winners[i], amount[i]);
+		for (auto j = i + 1; j < i + 1 + winners[i]; j++)
+		{
+			amount[j] += amount[i];
+		}
+	}
+
+	return std::accumulate(amount.begin(), amount.end(), 0);
 }
 
 } // namespace d04
--- aoc2023/test.cc
+++ aoc2023/test.cc
@@ -1,20 +1,22 @@
 
+#include "functions.h"
+
 #include <catch2/catch_test_macros.hpp>
 #include <spdlog/spdlog.h>
 
-#include "functions.h"
-
-TEST_CASE("D1P1") {
-  auto data = R"(1abc2
+TEST_CASE("D1P1")
+{
+	auto data = R"(1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet)";
 
-  REQUIRE(d01::p1(data) == 142);
+	REQUIRE(d01::p1(data) == 142);
 }
 
-TEST_CASE("D1P2") {
-  auto data = R"(two1nine
+TEST_CASE("D1P2")
+{
+	auto data = R"(two1nine
 eightwothree
 abcone2threexyz
 xtwone3four
@@ -22,34 +24,37 @@ xtwone3four
 zoneight234
 7pqrstsixteen)";
 
-  REQUIRE(d01::p2(data) == 281);
+	REQUIRE(d01::p2(data) == 281);
 }
 
-TEST_CASE("D2P1") {
-  auto data =
-      R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+TEST_CASE("D2P1")
+{
+	auto data =
+	    R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)";
 
-  REQUIRE(d02::p1(data) == 8);
+	REQUIRE(d02::p1(data) == 8);
 }
 
-TEST_CASE("D2P2") {
-  auto data =
-      R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+TEST_CASE("D2P2")
+{
+	auto data =
+	    R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)";
 
-  REQUIRE(d02::p2(data) == 2286);
+	REQUIRE(d02::p2(data) == 2286);
 }
 
-TEST_CASE("D3P1") {
-  auto data =
-      R"(467..114..
+TEST_CASE("D3P1")
+{
+	auto data =
+	    R"(467..114..
 ...*......
 ..35..633.
 ......#...
@@ -60,12 +65,13 @@ TEST_CASE("D3P1") {
 ...$.*....
 .664.598..)";
 
-  REQUIRE(d03::p1(data) == 4361);
+	REQUIRE(d03::p1(data) == 4361);
 }
 
-TEST_CASE("D3P2") {
-  auto data =
-      R"(467..114..
+TEST_CASE("D3P2")
+{
+	auto data =
+	    R"(467..114..
 ...*......
 ..35..633.
 ......#...
@@ -76,30 +82,32 @@ TEST_CASE("D3P2") {
 ...$.*....
 .664.598..)";
 
-  REQUIRE(d03::p2(data) == 467835);
+	REQUIRE(d03::p2(data) == 467835);
 }
 
-TEST_CASE("D4P1") {
-  spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-  auto data =
-      R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+TEST_CASE("D4P1")
+{
+	spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+	auto data =
+	    R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)";
 
-  REQUIRE(d04::p1(data) == 13);
+	REQUIRE(d04::p1(data) == 13);
 }
 
-TEST_CASE("D4P2") {
-  auto data =
-      R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+TEST_CASE("D4P2")
+{
+	auto data =
+	    R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)";
 
-  // REQUIRE(d04::p2(data) == 13);
+	REQUIRE(d04::p2(data) == 30);
 }
