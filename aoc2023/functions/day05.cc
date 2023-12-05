ce77c4ade5a90d38ac65a5f408c150525d38cba9
Rico Backasch <rico.backasch@fabmatics.com>
2023-12-05 20:45:54 +0100

    d5 bad solution

--- aoc2023/functions/day05.cc
+++ aoc2023/functions/day05.cc
@@ -1,27 +1,150 @@
 
+#include <algorithm>
+#include <array>
+#include <execution>
+#include <iterator>
+#include <limits>
 #include <sstream>
 #include <string>
+#include <string_view>
+#include <vector>
 
-namespace d05 {
+namespace d05
+{
 
-int p1(const char *data) {
-  std::stringstream stream{data};
-  std::string line;
-  int sum = 0;
-  while (std::getline(stream, line, '\n')) {
-    //sum += p1_get_num_of_line(line);
-  }
-  return sum;
+using seed_t = std::vector<unsigned long>;
+using map_t =
+    std::vector<std::tuple<unsigned long, unsigned long, unsigned long>>;
+
+void parse_seeds_p1(seed_t& seeds, std::string const& line)
+{
+	std::stringstream stream{ line };
+	std::string       tmp;
+	std::getline(stream, tmp, ' ');
+	while (std::getline(stream, tmp, ' ')) { seeds.push_back(std::stol(tmp)); }
 }
 
-int p2(const char *data) {
-  std::stringstream stream{data};
-  std::string line;
-  int sum = 0;
-  while (std::getline(stream, line, '\n')) {
-    //sum += p2_get_num_of_line(line);
-  }
-  return sum;
+void add_to_map(map_t& map, std::string const& line)
+{
+	std::stringstream stream{ line };
+	std::string       tmp;
+	std::getline(stream, tmp, ' ');
+	auto i1 = std::stol(tmp);
+	std::getline(stream, tmp, ' ');
+	auto i2 = std::stol(tmp);
+	std::getline(stream, tmp, ' ');
+	auto i3 = std::stol(tmp);
+
+	map.push_back({ i1, i2, i3 });
 }
 
-} // namespace d4
+unsigned long get_next_value(map_t const& map, unsigned long value)
+{
+	for (auto [dst, src, range] : map)
+	{
+		if (value >= src and value < src + range)
+		{
+			return dst + (value - src);
+		}
+	}
+
+	return value;
+}
+
+std::pair<seed_t, std::array<map_t, 8>> get_data(const char* data)
+{
+	seed_t               seeds;
+	std::array<map_t, 8> maps;
+
+	std::stringstream stream{ data };
+	std::string       line;
+
+	std::getline(stream, line, '\n');
+	parse_seeds_p1(seeds, line);
+
+	auto it = maps.begin();
+	bool in_map;
+	while (std::getline(stream, line, '\n'))
+	{
+		if (line.empty())
+		{
+			if (in_map)
+			{
+				in_map = false;
+				it++;
+			}
+			continue;
+		}
+		if (line.ends_with("map:"))
+		{
+			in_map = true;
+			continue;
+		}
+		add_to_map(*it, line);
+	}
+
+	return { seeds, maps };
+}
+
+unsigned process(seed_t seeds, std::array<map_t, 8> const& maps)
+{
+	std::transform(
+	    std::execution::par_unseq,
+	    seeds.cbegin(),
+	    seeds.cend(),
+	    seeds.begin(),
+	    [&](auto seed) {
+		    return get_next_value(
+		        maps[7],
+		        get_next_value(
+		            maps[6],
+		            get_next_value(
+		                maps[5],
+		                get_next_value(
+		                    maps[4],
+		                    get_next_value(
+		                        maps[3],
+		                        get_next_value(
+		                            maps[2],
+		                            get_next_value(
+		                                maps[1],
+		                                get_next_value(maps[0], seed))))))));
+	    });
+	auto min = std::ranges::min_element(seeds);
+	return *min;
+}
+
+int p1(const char* data)
+{
+	auto [seeds, maps] = get_data(data);
+
+	auto min = process(seeds, maps);
+
+	spdlog::info("curr min {}", min);
+	return static_cast<int>(min);
+}
+
+int p2(const char* data)
+{
+	auto [seeds, maps] = get_data(data);
+
+	seed_t seeds2;
+	size_t max_amount = 0;
+	for (auto i = 0; i < seeds.size(); i += 2) { max_amount += seeds[i + 1]; }
+
+	seeds2.reserve(max_amount);
+	for (auto i = 0; i < seeds.size(); i += 2)
+	{
+		auto start  = seeds[i];
+		auto amount = seeds[i + 1];
+		for (auto end = start + amount; start < end; start++)
+		{
+			seeds2.emplace_back(start);
+		}
+	}
+	auto min = process(seeds2, maps);
+
+	spdlog::info("curr min {}", min);
+	return static_cast<int>(min);
+}
+} // namespace d05
--- aoc2023/test.cc
+++ aoc2023/test.cc
@@ -27,7 +27,7 @@ zoneight234
 	REQUIRE(d01::p2(data) == 281);
 }
 
-TEST_CASE("D2P1")
+TEST_CASE("D2")
 {
 	auto data =
 	    R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -37,21 +37,10 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)";
 
 	REQUIRE(d02::p1(data) == 8);
-}
-
-TEST_CASE("D2P2")
-{
-	auto data =
-	    R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)";
-
 	REQUIRE(d02::p2(data) == 2286);
 }
 
-TEST_CASE("D3P1")
+TEST_CASE("D3")
 {
 	auto data =
 	    R"(467..114..
@@ -66,28 +55,11 @@ TEST_CASE("D3P1")
 .664.598..)";
 
 	REQUIRE(d03::p1(data) == 4361);
-}
-
-TEST_CASE("D3P2")
-{
-	auto data =
-	    R"(467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..)";
-
 	REQUIRE(d03::p2(data) == 467835);
 }
 
-TEST_CASE("D4P1")
+TEST_CASE("D4")
 {
-	spdlog::set_level(spdlog::level::debug); // Set global log level to debug
 	auto data =
 	    R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -97,17 +69,47 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)";
 
 	REQUIRE(d04::p1(data) == 13);
+	REQUIRE(d04::p2(data) == 30);
 }
 
-TEST_CASE("D4P2")
+TEST_CASE("D5")
 {
+	spdlog::set_level(spdlog::level::debug);
 	auto data =
-	    R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)";
+	    R"(seeds: 79 14 55 13
 
-	REQUIRE(d04::p2(data) == 30);
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4)";
+
+	REQUIRE(d05::p1(data) == 35);
+	REQUIRE(d05::p2(data) == 46);
 }
